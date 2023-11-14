11b1b5753db06c881796ae38069f927f64cccb77
rock's ezweb <alikerock@gmail.com>
2023-11-14 15:08:12 +0900

    first commit

---
+++ src/App.js
@@ -0,0 +1,86 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css';
+import Form from 'react-bootstrap/Form';
+import { useState, useEffect } from 'react';
+import Todo from './Todo';
+
+function App() {
+  const [todo, setTodo] = useState([]);
+  const [todoid, setTodoid] = useState(0);
+
+  const getTodoList = () =>{
+    console.log('getTodoList 실행');
+    const todoStrFromStorage = window.localStorage.getItem('todo');
+    console.log(todoStrFromStorage);
+    if(todoStrFromStorage !== null){
+      const todoObj = JSON.parse(todoStrFromStorage);
+      setTodo(todoObj);
+    }
+  }
+  const setStorage = () =>{
+    console.log('setStorage 실행');
+    const todoString = JSON.stringify(todo);
+    window.localStorage.setItem('todo', todoString);
+  }
+  useEffect(()=>{
+    getTodoList();
+  },[])
+  useEffect(()=>{
+    setStorage(); 
+  },[todo])
+
+  const deleteTodo = (id) =>{
+    let newTodos = [...todo];
+    let index = newTodos.findIndex(item => (item.id === id));
+    newTodos.splice(index,1);
+    setTodo(newTodos);
+  }
+  const setChecked = (id, check) =>{
+    console.log(id,check);
+    let newTodos = [...todo];
+    let index = newTodos.findIndex(item => (item.id === id));
+    newTodos[index] = {id:id, text:newTodos[index].text, check};
+    setTodo(newTodos);
+  }
+  const update = (id, val) =>{
+    console.log(id,val);
+    let newTodos = [...todo];
+    let index = newTodos.findIndex(item => (item.id === id));
+    newTodos[index] = {id:id, text:val, checked:newTodos[index].checked};
+    setTodo(newTodos);
+  }
+
+  let todos = todo.map((item)=>(
+      <Todo key={item.id} data={item} deleteTodo={deleteTodo} setChecked={setChecked} update={update} />
+    )
+  )
+  let addTodo = (value)=>{
+    let newTodos = [...todo];
+    let newId = todoid + 1;
+    setTodoid(newId);
+    newTodos.push({id:newId, text:value, checked:false});
+    setTodo(newTodos);
+    document.getElementById('todo').value = '';
+  }
+  console.log(todo);
+  return (
+    <div className="App">
+      <h1>Todo List</h1>
+      <Form onSubmit={(e)=>{
+        e.preventDefault();        
+        addTodo(e.target.todo.value);
+      }}>
+        <Form.Group className="mb-3 d-flex align-items-center gap-3" controlId="todo">
+          <Form.Label>Todo Input</Form.Label>
+          <Form.Control type="text" name="todo" placeholder="할일을 입력하세요" />
+        </Form.Group>      
+      </Form>
+      <hr/>
+      <div>
+        {todos}
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Todo.js
@@ -0,0 +1,58 @@
+import React, { useState } from "react";
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
+
+const Todo = ({data, deleteTodo, setChecked, update})=>{
+  const [mode, setMode] = useState('read');
+  const [text, setText] = useState(data.text);
+  const [isChecked, setIsChecked] = useState(false);
+
+  let className = 'd-flex gap-2';
+  let formClass = 'hidden';
+
+  const handleCheckbox = ()=>{
+    setIsChecked(!isChecked);
+    setChecked(data.id, !isChecked);
+  }
+  console.log(isChecked);
+  const todoDelete = ()=>{
+    deleteTodo(data.id);
+  }
+  if(mode === 'edit'){
+    className +=' hidden';
+    formClass = '';
+  }
+  const handleEdit = (val)=>{
+    setText(val);
+  }
+  const updateTodo = (e)=>{
+    e.preventDefault();
+    update(data.id, text);
+    setMode('read');
+  }
+  return(
+    <div>
+      <Form.Check id={`check-api-${data.id}`} className={className}>
+        <Form.Check.Input type="checkbox" id={`check-api-${data.id}`} onClick={handleCheckbox}/>
+        <Form.Check.Label>{data.text}</Form.Check.Label>
+        <Button variant="danger" size="sm" onClick={todoDelete}>Delete</Button>        
+        <Button variant="info" size="sm" onClick={()=>{
+           setMode('edit');
+          }}>Edit</Button>        
+      </Form.Check>
+      <Form className={formClass} onSubmit={updateTodo}>
+        <Form.Group className="mb-3 d-flex gap-2" controlId={`edit-todo-${data.id}`}>          
+          <Form.Control type="text"  value={text}  onChange={(e)=>{
+            handleEdit(e.target.value);
+          }}/>
+          <Button type="submit" variant="secondary">Update</Button>
+          <Button variant="secondary" onClick={()=>{
+           setMode('read');
+          }}>cancel</Button>
+        </Form.Group>       
+      </Form>     
+      
+    </div>
+  )
+}
+export default Todo;
